015d6dee82c90a29ca1620233de2e2ef54bc0018
Daniel Beffermann <dbeffermannj@gmail.com>
2023-06-25 23:55:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -50,9 +50,8 @@ with st.form("my_form", clear_on_submit=True):
     submit_btn = st.form_submit_button("Enviar Respuestas")
 
 if submit_btn:
-    #st.sidebar.write(['Correct' in i for i in st.session_state.respuestas.get(question.question)])
-    #st.sidebar.write([a,b,c,d,e,f])
-    st.sidebar.write(st.session_state)
+    st.sidebar.write(['Correct' in i for i in st.session_state.respuestas.get(question.question)])
+    st.sidebar.write([a,b,c,d,e,f])
     #if ['Correct' in i for i in st.session_state.respuestas.get(question.question)] == [a,b,c,d,e,f]:
     #    st.sidebar.success("Correcto :)")
     #else:
